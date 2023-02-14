800ddfcf076034234f8cd3a05076b725e6e35fa3
Paul Campbell <paul.campbell.nottingham@gmail.com>
2023-02-14 13:53:17 +0000

    completed implementation

--- PrimeService/PrimeService.cs
+++ PrimeService/PrimeService.cs
@@ -4,13 +4,21 @@ namespace Prime.Services
 {
     public class PrimeService
     {
-        public bool IsPrime(int candidate)
+       public bool IsPrime(int candidate)
         {
-            if (candidate == 1)
+            if (candidate < 2)
             {
                 return false;
             }
-            throw new NotImplementedException("Not fully implemented.");
+
+            for (var divisor = 2; divisor <= Math.Sqrt(candidate); divisor++)
+            {
+                if (candidate % divisor == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
