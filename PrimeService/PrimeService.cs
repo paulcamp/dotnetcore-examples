64e43cced54554950deabb8e6e59c95e3149958d
Paul Campbell <paul.campbell.nottingham@gmail.com>
2023-02-14 13:45:23 +0000

    corrected indentation

--- PrimeService/PrimeService.cs
+++ PrimeService/PrimeService.cs
@@ -4,13 +4,13 @@ namespace Prime.Services
 {
     public class PrimeService
     {
-      public bool IsPrime(int candidate)
-{
-    if (candidate == 1)
-    {
-        return false;
-    }
-    throw new NotImplementedException("Not fully implemented.");
-}
+        public bool IsPrime(int candidate)
+        {
+            if (candidate == 1)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Not fully implemented.");
+        }
     }
 }
